e92f7dac05372a2005e79e0c5338d050e65b6d6a
Craig Neff <ifly12610@gmail.com>
2021-04-12 21:33:17 -0600

    trying to make a new branch from the main with index.js changed and the readme.md changed.

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$.get("https://disease.sh/v3/covid-19/all", (cases) => display(cases));
+function display(cases) {
+  console.log(cases);
+  for (let i = 0; i < cases.length; i++) {
+    let cases = `<tr><td>${cases[i].cases}</td><td>${cases[i].recovered}</td><td>${cases[i].deaths}</td><td>${cases[i].tests}</td></tr>`;
+    $("#covid-table").append(cases);
+  }
+}
