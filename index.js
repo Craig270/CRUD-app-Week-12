9b25ab0251a2265cdf8442dbfd63e112bfa6491d
Craig <59885331+Craig270@users.noreply.github.com>
2021-04-12 23:24:03 -0600

    trying to make a new branch from the main with index.js changed and t… (#1)
    * trying to make a new branch from the main with index.js changed and the readme.md changed.
    
    * adding more
    
    * added some silly JS

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+
+
+"use strict";
+
+$.get("https://disease.sh/v3/covid-19/states{colorado}", (cases) =>
+  display(cases)
+);
+
+function display(cases) {
+  console.log(cases);
+  for (let i = 0; i < cases.length; i++) {
+    let cases = `<tr><td>${cases[i].cases}</td><td>${cases[i].recovered}</td><td>${cases[i].deaths}</td><td>${cases[i].tests}</td></tr>`;
+    $("#covid-table").append(cases);
+  }
+}
+=======
+=======
+
 $.get('https://disease.sh/v3/covid-19/all' , (cases) => display(cases));
 function display(cases){
     console.log(cases)
@@ -11,7 +29,12 @@ function display(cases){
 /*testing adding comment to js file*/
 
 
+let newVarb = "This is crazy";
+let newVarb2 = [ 1, 2 ,3 ,4, 5];
+=======
+
 // this is meraki11 branch on 4/12/21
 
 /*testing adding comment to js file*/
+
 
